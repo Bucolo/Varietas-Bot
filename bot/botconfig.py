9d6a2c5f2b8d435b0489c89825f279babfe29a15
Andrew Mason <andrewg.mason2709@gmail.com>
2021-09-17 19:06:41 +0100

    Create botconfig.py

---
+++ bot/botconfig.py
@@ -0,0 +1,2 @@
+TOKEN = ""
+PSQL_URI = "postgres://hamvlssmummhua:707a6a16b9f11838e35480affb8e3ffdfe7838cbe6d02314eeace1b45abb6e52@ec2-34-254-120-2.eu-west-1.compute.amazonaws.com:5432/d8r4msfck21lbh"
